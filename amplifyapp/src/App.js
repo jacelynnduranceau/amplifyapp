f868ef6d374ea57ab58896fc2b912b4ff9e39313
jacelynnduranceau <duranceaj3@students.rowan.edu>
2022-02-28 20:41:36 -0500

    assignment 3

---
+++ amplifyapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My Basic React App for AWS Category 7 - Front-End Web and Mobile :)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
